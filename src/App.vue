ed4e2d597b253a424bf601b0b0319f276500b61e
JinweiOS <jinwei.peng@beingthink.com>
2023-10-22 16:40:05 +0800

    feat: webpack完整的生成环境打包

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,26 @@
-<template><div class="title font-big">hello</div></template>
+<template>
+  <div class="title font-big">hello</div>
+  <img :src="url" />
+  <img :src="pubUrl" />
+</template>
+<script>
+import {ref} from 'vue'
+import testImg from './assets/test.png'
+import otherImg from './assets/other.jpg'
+export default {
+  setup() {
+    const url = ref(testImg)
+    setTimeout(() => {
+      url.value = otherImg
+    }, 3000)
+    const pubUrl = ref('/vite.svg')
+    return {
+      url,
+      pubUrl
+    }
+  }
+}
+</script>
 <style lang="scss">
 .font-big {
   font-size: 60px;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 import path from 'node:path'
 import process from 'node:process'
 import { VueLoaderPlugin } from 'vue-loader'
-import MiniCssExtractPlugin  from 'mini-css-extract-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
+import {CleanWebpackPlugin } from 'clean-webpack-plugin'
+import webpack from 'webpack';
+const { ProgressPlugin } = webpack;
 
 const config = {
   entry: './src/main.js',
   output: {
     path: path.join(process.cwd(), 'dist'),
-    filename: 'bundle.js'
+    filename: 'js/[name].[fullhash:8].js'
   },
   mode: 'production',
   module: {
@@ -16,12 +21,53 @@ const config = {
       { test: /\.vue$/, loader: 'vue-loader' },
       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
       // sass-loader如果想能够正常工作，需要安装sass工具包
-      { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] }
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      // 处理图片
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        type: 'asset',
+        generator: {
+          filename: 'img/[name].[hash:8][ext]'
+        }
+      },
+      // 处理音视频文件
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        type: 'asset',
+        generator: {
+          filename: 'media/[name].[hash:8][ext]'
+        }
+      },
+      // 处理字体文件
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        type: 'asset',
+        generator: {
+          filename: 'fonts/[name].[hash:8][ext]'
+        }
+      }
     ]
   },
   plugins: [
     new VueLoaderPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[fullhash:8].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public'
+        }
+      ]
+    }),
+    new CleanWebpackPlugin(),
+    new ProgressPlugin()
   ]
 }
 
